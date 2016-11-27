16003b8ee9ad0b60a79475f1ae95879776d674a4
System Administrator <ivoworks@gmail.com>
2016-11-27 20:57:13 +0100

    updated bookModel

--- models/bookModel.js
+++ models/bookModel.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var bookModel = new Schema({
-    title: {
-        type: String
-    },
-    author: {type: String},
-    genre: {type: String},
-    read: {type: Boolean, default:false}
+    items: [{
+        title: {type: String},
+        author: {type: String},
+        genre: {type: String},
+        read: {type: Boolean, default: false}
+    }],
+    _links:{},
+    pagination{}
 });
 
-module.exports= mongoose.model('Book', bookModel);
+module.exports = mongoose.model('Book', bookModel);
