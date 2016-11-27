5039dcbfca281a710ec5cddf197cc12a4bd5be4e
System Administrator <ivoworks@gmail.com>
2016-11-27 19:48:50 +0100

    updated header options

--- app.js
+++ app.js
@@ -20,9 +20,10 @@ app.use(bodyParser.json());
 
 bookRouter = require('./Routes/bookRoutes')(Book);
 
-app.use(function (req, res) {
-   res.header("Access-Control-Allow-Origin", "*") ;
+app.use(function (req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
 });
 
 app.use('/api/books', bookRouter);
