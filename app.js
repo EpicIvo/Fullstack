e57d99d1f013db8e791839a68d0e40e22a8ec634
System Administrator <ivoworks@gmail.com>
2016-11-27 17:17:56 +0100

    New ip adresses added

--- app.js
+++ app.js
@@ -2,38 +2,29 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
-
 var db;
 console.log('Hello');
-if(process.env.ENV == 'Test'){
-
-    db = mongoose.connect('mongodb://ds111718.mlab.com:11718/fullstack');
+if (process.env.ENV == 'Test') {
+    db = mongoose.connect('mongodb://ivo:password@ds111718.mlab.com:11718/fullstack');
 }
-
-else{
-    db= mongoose.connect('mongodb://ds111718.mlab.com:11718/fullstack');
+else {
+    db = mongoose.connect('mongodb://ivo:password@ds111718.mlab.com:11718/fullstack');
 }
 
 var Book = require('./models/bookModel');
-
 var app = express();
-
 var port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 bookRouter = require('./Routes/bookRoutes')(Book);
 
-
-app.use('/api/books', bookRouter); 
-
-
-app.get('/', function(req, res){
+app.use('/api/books', bookRouter);
+app.get('/', function (req, res) {
     res.send('welcome to my API!');
 });
-
-app.listen(port, function(){
+app.listen(port, function () {
     console.log('Gulp is running my app on  PORT: ' + port);
 });
 
