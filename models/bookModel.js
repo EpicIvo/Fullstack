8114623b64818a569f88a909fea115f0dccb8f90
System Administrator <ivoworks@gmail.com>
2016-11-27 21:53:46 +0100

    updated header

--- models/bookModel.js
+++ models/bookModel.js
@@ -2,14 +2,14 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var bookModel = new Schema({
+    _links:{},
+    pagination:{},
     items: [{
         title: {type: String},
         author: {type: String},
         genre: {type: String},
         read: {type: Boolean, default: false}
-    }],
-    _links:{},
-    pagination:{}
+    }]
 });
 
 module.exports = mongoose.model('Book', bookModel);
