a2376bbad603a3106e07a53064b137c52e639f8d
System Administrator <ivoworks@gmail.com>
2016-12-05 21:41:18 +0100

    Changed headers

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -9,7 +9,7 @@ var routes = function (Book) {
         .get(bookController.get)
 
         .options(function (err, res) {
-            res.header('Accept', 'applicaton/json');
+            res.header('Accept', 'application/json');
             res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS,');
             res.send(200)
         });
