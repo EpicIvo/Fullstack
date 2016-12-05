78788d9d9cfe15d5d7488fd60cd6264cc6d5374f
System Administrator <ivoworks@gmail.com>
2016-12-05 21:01:48 +0100

    Changed headers

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(bodyParser.json({
 bookRouter = require('./Routes/bookRoutes')(Book);
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
     next();
 });
 
