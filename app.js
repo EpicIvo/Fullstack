4ff5f5c1362cc7c05d37426ef48fcd08961aad15
System Administrator <ivoworks@gmail.com>
2016-12-05 20:34:31 +0100

    Changed headers

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ bookRouter = require('./Routes/bookRoutes')(Book);
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
     res.header('Access-Control-Allow-Origin', 'Content-type');
     next();
 });
