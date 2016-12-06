a95fdacf34607e887b3009b25d04f72799f9d2d1
System Administrator <ivoworks@gmail.com>
2016-12-06 06:24:47 +0100

    added accepts json

--- app.js
+++ app.js
@@ -32,6 +32,14 @@ app.use(bodyParser.json({
 
 bookRouter = require('./Routes/bookRoutes')(Book);
 
+app.use(function (req, res) {
+    if (!req.accepts('json')) {
+        return res.status(400).json({message: 'Accepted format is application/json'});
+    }else {
+        res.status(200).send();
+    }
+});
+
 app.use('/api/books', bookRouter);
 app.get('/', function (req, res) {
     res.send('welcome to my <a href="http://localhost:8080/api/books">API!</a>');
