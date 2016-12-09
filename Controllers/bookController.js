fc4f272df4b7ae5d343d24c0743d46863fd79569
System Administrator <ivoworks@gmail.com>
2016-12-09 15:10:22 +0100

    Working on pagination

--- Controllers/bookController.js
+++ Controllers/bookController.js
@@ -1,54 +1,52 @@
-var ObjectID = require('mongodb').ObjectID;
-
 var bookController = function (Book) {
     var post = function (req, res) {
-        var objectId = new ObjectID();
         var book = new Book();
-        book.items = [];
-        book.items.push({
-            _id: objectId,
-            author: req.body.author,
-            genre: req.body.genre,
-            title: req.body.title
-        });
-        if (!req.body.title) {
-            res.status(400);
-            res.send('Title is required');
-        }
-        else {
-            book.save();
-            res.status(201);
-            res.send(book);
-        }
+        book.author = req.body.author;
+        book.genre = req.body.genre;
+        book.title = req.body.title;
     };
-    var get = function (req, res) {
-        var currentPage = req.query.currentPage;
-        var pageSize = req.query.pageSize;
-
-        var query = {};
-        if (req.query.genre) {
-            query.genre = req.query.genre;
-        }
-        Book.find(query, {}, {limit: 10}, function (err, books) {
-            if (err) {
-                res.status(500).send(err);
-            }
-            else {
-                var returnBooks = [];
-            }
-            books.forEach(function (element, index, array) {
-                var newBook = element.toJSON();
-                newBook.items._links = {};
-                newBook.items._links.self = 'http://' + req.headers.host + '/api/books/' + newBook._id;
-                returnBooks.push(newBook);
-            });
-            res.json(returnBooks);
-        });
-    };
-    return {
-        post: post,
-        get: get
+    if (!req.body.title || !req.body.genre || !req.body.author) {
+        res.status(400);
+        res.send('Title is required');
     }
+    else {
+        book.save();
+        res.status(201);
+        res.send(book);
+    }
+};
+var get = function (req, res) {
+    var EpicResponseObject = {
+        items: [],
+        _links: {},
+        vagination: {}
+    };
+    var query = {};
+    if (req.query.genre) {
+        query.genre = req.query.genre;
+    }
+    Book.find(query, {}, {limit: 10}, function (err, books) {
+        if (err) {
+            return res.status(500).send(err);
+        }
+        books.forEach(function (element, index, array) {
+            var newBook = element.toJSON();
+            newBook.items._links = {};
+            newBook.items._links.self = 'http://' + req.headers.host + '/api/books/' + newBook._id;
+            EpicResponseObject.items.push(newBook);
+        });
+
+        // Commence links
+
+
+        // Initiate vagination
+
+        res.json(EpicResponseObject);
+    });
+};
+return {
+    post: post,
+    get: get
 };
 
 module.exports = bookController;
