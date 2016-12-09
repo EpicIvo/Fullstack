a4be571ff5b8a53dd4465f0bfd012e3b21cfcc1b
System Administrator <ivoworks@gmail.com>
2016-12-09 16:50:56 +0100

    Mongoose editation

--- models/bookModel.js
+++ models/bookModel.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var bookModel = new Schema({
-    _id: {type: String},
     title: String,
     author: String,
     genre: String
