4abf6cf463438e5f4b188b27a1ff1cadeb770013
EpicIvo <ivoworks@gmail.com>
2017-01-24 18:11:06 +0100

    Changed allow header

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ app.use(function (req, res, next) {
     res.header('Accept', 'application/json');
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Header', 'Content-Type, header');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Allow', 'GET, POST, DELETE');
+    res.header('Allow', 'GET, POST, PUT, PATCH, DELETE');
     if (!req.accepts('json')) {
         res.status(400);
         return res.send("Only json is accepted");
