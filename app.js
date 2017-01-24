b6cfa850bae011e366cc7d321ab7d730fd038e20
EpicIvo <ivoworks@gmail.com>
2017-01-24 21:14:27 +0100

    Changed allow header

--- app.js
+++ app.js
@@ -11,12 +11,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.header('Accept', 'application/json');
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Header', 'Accept, Accept-Language, Content-Language, Content-Type');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEADER');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Allow', 'GET, POST, OPTIONS, HEADER');
+    // res.header('Accept', 'application/json');
+    // res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Header', 'Accept, Accept-Language, Content-Language, Content-Type');
+    // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEADER');
+    // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    // res.header('Allow', 'GET, POST, OPTIONS, HEADER');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", false);
+    res.header("Access-Control-Max-Age", "86400");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
     if (!req.accepts('json')) {
         res.status(400);
         return res.send("Only json is accepted");
