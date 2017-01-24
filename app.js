0f4b52cc965b9469b119c4b7732ef20081ea373f
EpicIvo <ivoworks@gmail.com>
2017-01-24 13:02:37 +0100

    Changed allow header

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', 'https://fullstack-s.herokuapp.com/api/movies');
     res.header('Access-Control-Allow-Header', 'Content-Type, header');
     res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, HEADERS');
-    res.header('Allow', 'POST, GET, PUT, PATCH, HEADERS');
     if (!req.accepts('json')) {
         res.status(400);
         return res.send("Only json is accepted");
