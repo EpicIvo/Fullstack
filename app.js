cbc49cdd6704db09e90a21d1e5fdd4f1d91e5f08
EpicIvo <ivoworks@gmail.com>
2017-01-25 13:13:03 +0100

    Changed allow header

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use(function (req, res, next) {
     // res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, HEADER');
     // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
-    res.header('Allow', 'GET, POST, OPTIONS, HEADER');
+    res.header('Allow', 'GET, PUT, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, HEADER");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Credentials", false);
     res.header("Access-Control-Max-Age", "86400");
     res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
